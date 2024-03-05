ac27cd14f28de5c9c26d2210780b2593db595398
Valentina Rozo <varo07@outlook.com>
2024-03-04 20:06:49 -0500

    s

---
+++ ComercianteEspacial/src/main/java/com/proyecto/ComercianteEspacial/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.proyecto.ComercianteEspacial.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String Home(){
+        return "menu";
+    }
+}
