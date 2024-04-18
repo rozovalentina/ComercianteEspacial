237c4cbe7fc2c3893eebd5e1db9f26929a5669dc
Daniel Florido <d.florido@javeriana.edu.co>
2024-04-18 00:50:42 -0500

    sad

--- ComercianteEspacial/src/main/java/com/proyecto/ComercianteEspacial/CorsConfiguration.java
+++ ComercianteEspacial/src/main/java/com/proyecto/ComercianteEspacial/CorsConfiguration.java
@@ -1,4 +1,5 @@
-import org.springframework.context.annotation.Bean;
+package com.proyecto.ComercianteEspacial;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class CorsConfiguration implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
+    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:4200") // Permite solicitudes solo desde este origen
                 .allowedMethods("*") // Métodos HTTP permitidos
