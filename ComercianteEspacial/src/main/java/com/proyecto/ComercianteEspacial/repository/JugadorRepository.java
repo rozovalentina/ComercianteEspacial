48265d653c3952ff3525e39b0a253bf48fae019f
Daniel Florido <d.florido@javeriana.edu.co>
2024-04-18 13:36:54 -0500

    fix

--- ComercianteEspacial/src/main/java/com/proyecto/ComercianteEspacial/repository/JugadorRepository.java
+++ ComercianteEspacial/src/main/java/com/proyecto/ComercianteEspacial/repository/JugadorRepository.java
@@ -3,6 +3,7 @@ package com.proyecto.ComercianteEspacial.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.proyecto.ComercianteEspacial.model.Jugador;
@@ -11,5 +12,6 @@ import com.proyecto.ComercianteEspacial.model.Jugador;
 public interface JugadorRepository extends JpaRepository<Jugador, Long> {
     @SuppressWarnings("null")
     Page<Jugador> findAll(Pageable pageable);
-    Jugador findbyName(String name);
+    @Query("SELECT j FROM Jugador j WHERE j.nombre LIKE %:nombre%")
+    Jugador findbyName(String nombre);
 }
